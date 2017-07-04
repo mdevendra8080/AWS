a2fc9389f4cbb0b1dec6a69603edd9af8f347f16
mdevendra8080 <devendra.mhatre@capgemini.com>
2017-07-04 15:36:37 +0530

    Update CreateTablesLoadData.java

--- DynamoDB_Java_Project/src/main/java/DynamoDB/sample/CreateTablesLoadData.java
+++ DynamoDB_Java_Project/src/main/java/DynamoDB/sample/CreateTablesLoadData.java
@@ -31,10 +31,10 @@ public class CreateTablesLoadData {
     static SimpleDateFormat dateFormatter = new SimpleDateFormat(
             "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
 
-    static String productCatalogTableName = "ProductCatalog";
-    static String forumTableName = "Forum";
-    static String threadTableName = "Thread";
-    static String replyTableName = "Reply";
+    static String productCatalogTableName = "ProductCatalogDM";
+    static String forumTableName = "ForumDM";
+    static String threadTableName = "ThreadDM";
+    static String replyTableName = "ReplyDM";
 
     public static void main(String[] args) throws Exception {
 
